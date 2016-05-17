dfe3d13b428e2b18db96a7d60b5059c931afb32e
amespineira <amespineira@me.com>
2016-05-17 13:43:14 -0600

    Solved isogram

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,31 @@
+function Isogram(word){
+  this.word=word
+}
+Isogram.prototype.isIsogram=function(){
+  var toArray=this.removeNonAlphabet(this.word.toLowerCase()).split("");
+  var checker={};
+  var isIsogram=true;
+  for(var i=0; i<toArray.length; i++)
+  {
+    if(checker[toArray[i]]==='used')
+    {
+      return false;
+    }
+    checker[toArray[i]]='used';
+  }
+  return true;
+}
+Isogram.prototype.removeNonAlphabet=function(input){
+    var toArray=input.split("")
+    var output="";
+    for(var i=0; i<toArray.length; i++)
+    {
+      if(toArray[i]!="-"&&toArray[i]!=" ")
+      {
+        output+=toArray[i];
+      }
+    }
+    return output
+
+}
+module.exports= Isogram;
